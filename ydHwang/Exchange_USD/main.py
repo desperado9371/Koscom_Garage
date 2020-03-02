735757bde970b2915e8a2b401c70a17aa7f58574
yundong-h <61396719+yundong-h@users.noreply.github.com>
2020-03-02 11:04:05 +0900

    Update main.py

--- ydHwang/Exchange_USD/main.py
+++ ydHwang/Exchange_USD/main.py
@@ -1,6 +1,16 @@
 import Exchange_USD
+import pymysql
+from datetime import datetime
 
 
 if __name__ == '__main__':
     print("시작")
-    Exchange_USD.Get_ExchangUSD()
+    zTodayDate = datetime.today().strftime("%Y%m%d")
+    zExchangeUSD = Exchange_USD.Get_ExchangUSD()
+    conn = pymysql.connect(host="localhost", user="root", passwd="root", db="db_garage", charset ="utf8")
+    curs = conn.cursor(pymysql.cursors.DictCursor)
+
+    # ==== insert example ====
+    sql = "insert into exchange_rate(base_dt,coin_type,exchange_rate) values (%s, %s, %s)"
+    curs.execute(sql, (zTodayDate, 'bitcoin', float(zExchangeUSD)))
+    conn.commit()
