96d7250dde7661610c5c375d7fe7e67cb337abc8
ChoongHoon Lee <ch930701@gmail.com>
2020-03-19 15:10:41 +0900

    백테스트 샘플페이지 생성 및 연결 완료

--- Project/garage_web/garage/views.py
+++ Project/garage_web/garage/views.py
@@ -3,7 +3,12 @@ from django.shortcuts import redirect
 from django.contrib.auth.models import User
 from django.contrib import auth
 from django.views.decorators.csrf import csrf_exempt
-
+from binance.client import Client
+from datetime import datetime
+import numpy as np
+import pandas as pd
+import sys
+import ta
 
 # Create your views here.
 
@@ -49,10 +54,49 @@ def logout(request):
     return redirect('/')
 
 
+
 def backtest(request):
-    return render(request, 'garage/backtest.html',{})
+    # client = Client('ng0Zhq6ea42X3QxMV2RAubZxs508gguTISRwM13lQFFPrDDTxRiqmq3pBvIcvJMy',
+    #                 'vHZDWuQvPf4mcaDvzxwRbtIWDbWuCyFyyG59bCZeTW6A6sd98qbHfCsFDVDdN3wn')
+    # klines = client.get_historical_klines("BTCUSDT", Client.KLINE_INTERVAL_1DAY, "1 month ago UTC")
+    #
+    #
+    #
+    # unix_timestamps = []
+    # for kline in klines:
+    #     unix_timestamps.append(kline[0])
+    #
+    # timestamps = []
+    #
+    # closed_prices = []
+    # open_prices = []
+    # high_prices = []
+    # low_prices = []
+    # volume = []
+    # print(sys.path)
+    # df = pd.DataFrame(list(zip(open_prices, closed_prices, high_prices, low_prices, volume)), index=timestamps,
+    #                   columns=['open', 'close', 'high', 'low', 'volume'])
+    #
+    # for kline in klines:
+    #     closed_prices.append(float(kline[4]))
+    #     open_prices.append(float(kline[1]))
+    #     high_prices.append(float(kline[2]))
+    #     low_prices.append(float(kline[3]))
+    #     volume.append(float(kline[5]))
+    #
+    # for unix_timestamp in unix_timestamps:
+    #     timestamps.append(datetime.fromtimestamp(unix_timestamp / 1000).strftime("%m/%d/%Y"))
+    #
+    # for kline in klines:
+    #     unix_timestamps.append(kline[0])
+    #
+    # candledata = []
+    # for i in range(len(timestamps)):
+    #     candledata.append([timestamps[i],low_prices[i],open_prices[i],closed_prices[i],high_prices[i]])
+    #
+    # print(candledata)
 
-
-
+    return render(request, 'garage/backtest.html', {'data': [10, 20, 30, 20, 40, 30, 50],
+                                                   'labels': [1, 2, 3, 4, 5, 6, 7]})
 
 
