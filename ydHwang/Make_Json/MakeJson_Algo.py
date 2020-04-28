f077ec28c6b7670ba25faa47c805041103e4b620
yundong-h <61396719+yundong-h@users.noreply.github.com>
2020-04-28 13:27:30 +0900

    upd
    upd

--- ydHwang/Make_Json/MakeJson_Algo.py
+++ ydHwang/Make_Json/MakeJson_Algo.py
@@ -5,11 +5,13 @@ Block1 = dict()
 Block2 = dict()
 
 #소분류
+obv=dict()
 MACD = dict()
 CCI = dict()
 NUM = dict()
 Group1 = dict()
 Group2 = dict()
+Group3 = dict()
 SIG = dict()
 Algo = dict()
 
@@ -21,12 +23,12 @@ Algo["block2"]=Block2
 
 ####################################
 Block1["min"]="1"
-Block1["max"]="1"
-Block1["total_count"]="1"
-Block1["group"]= [MACD,SIG,CCI]
+Block1["max"]="2"
+Block1["total_count"]="2"
+Block1["group"]= [Group1,Group2]
 
-#Group1["INDI"] = [MACD,SIG,CCI]
-
+Group1["group1"] = [MACD,SIG,CCI]
+Group2["group2"] = [MACD,SIG,NUM]
 
 MACD["name"] = "macd"
 MACD["val"] = {"input_close":"50000","input_n_fast":"60","input_n_slow":"30","input_n_sign":"5"}
@@ -44,14 +46,16 @@ CCI["val"] = {"volum":"10"}
 Block2["min"]="1"
 Block2["max"]="1"
 Block2["total_count"]="1"
-Block2["group"]= [MACD,SIG,NUM]
+Block2["group"]= [Group3]
 
-MACD["name"] = "macd"
-MACD["val"] = {"input_close":"50000","input_n_fast":"60","input_n_slow":"30","input_n_sign":"5"}
+Group3["group1"] = [obv,SIG,NUM]
 
 SIG["name"] = "sig"
 SIG["val"] = ">"
-
+#############################
+obv["name"] = "obv"
+obv["val"] = {"volum":"10"}
+#############################
 NUM["name"] = "num"
 NUM["val"] = "20000"
 
